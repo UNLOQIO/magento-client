b013951666e264ae2763bf3d6e81bb6f0d634015
Diana Botean <diana.botean@evozon.com>
2016-07-27 12:06:22 +0300

    Upgrade file small edit

--- app/code/local/Unloq/Login/sql/unloqlogin_setup/upgrade-0.1.0-0.1.1.php
+++ app/code/local/Unloq/Login/sql/unloqlogin_setup/upgrade-0.1.0-0.1.1.php
@@ -3,9 +3,10 @@ $installer = $this;
 
 $installer->startSetup();
 
+// add new column to admin_user table in order to keep the UNLOQ.io id for the admin user
 $installer->getConnection()->addColumn($installer->getTable('admin/user'), 'unloq_id', array(
     'type' => Varien_Db_Ddl_Table::TYPE_TEXT,
-    'length' => 256,
+    'length' => 255,
     'nullable' => true,
     'default' => "",
     'comment' => 'UNLOQ.io user id'
